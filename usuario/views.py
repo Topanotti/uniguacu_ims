09db41afdcb0bdf95d739cddf86dba9038c6a079
Vinicius Topanotti <vinicius.topanotti@gmail.com>
2024-02-15 10:18:07 -0300

    regisro

--- usuario/views.py
+++ usuario/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
 def home(request):
     
-    return render(request, 'home.html', {})
+    #verificar se o usuário está logado:
+    if request.method == 'POST':
+        email = request.POST['input_email']
+        password = request.POST['input_password']
+        user = authenticate(request, username=email, password=password) 
+        if user is not None:
+            login(request, user)
+            messages.sucess(request, "Logado com Sucesso!")
+            return redirect('home')
+        else:
+            messages.error(request, "Erro no login, tente novamente")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+    
+
+def register_user(request):
+    return render(request, 'register.html', {})
 
 def login_user(request):
     pass
 
 def logout_user(request):
-    pass
+    logout_user(request)
+    messages.success(request, "Você foi deslogado")
+    return redirect('home')
